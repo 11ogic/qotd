8f7d5d02357e6f02456c19eb869ef277b397177f
haven <haven@hellotalk.cn>
2024-07-29 21:47:17 +0800

    feat: pointer

---
+++ playground/pointer_practice.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    srand(time(NULL));
+
+    int matrix[4][3] = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9},
+            {10,11,12}
+    };
+
+    int i = rand() % 4, j = rand() % 3;
+    printf("matrix[%d][%d] = %d \n", i, j, matrix[i][j]);
+    printf("matrix[%d][%d] = %p \n", i, j, &matrix[i][j]);
+
+    int *p = matrix;
+//    printf("%d \n", *(p + i * (sizeof(matrix[0]) / sizeof(matrix[0][0])) + j));
+//    printf("%p \n", p + i * (sizeof(matrix[0]) / sizeof(matrix[0][0])) + j);
+    printf("%d \n", *(*(matrix + i) + j));
+    printf("%p \n", *(matrix + i) + j);
+
+    return 0;
+}
